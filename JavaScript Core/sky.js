cf3baf796d1a4a5fe026b7b8e862584297cf6eb9
Lalit Patil <lp613408@gmail.com>
2025-04-09 23:13:36 +0530

    Javascript core

---
+++ JavaScript Core/sky.js
@@ -0,0 +1,11 @@
+/* example on defining external script */
+function myfun1()
+{
+  document.write("hi, i am fun1");
+}
+
+function myfun2()
+{
+  document.write("hi, i am fun2");
+}
+/* save this program with a name "sky.js" in current location*/
