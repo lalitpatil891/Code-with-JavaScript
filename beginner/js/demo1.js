8faa9e21c6289ac5198ed4d1c895a22b9cd5a38a
Lalit Patil <lp613408@gmail.com>
2025-05-27 23:29:13 +0530

    demo

--- beginner/js/demo1.js
+++ beginner/js/demo1.js
@@ -1,27 +1,6 @@
-//area of a triangle using Heron’s formula,
 
+function generateOTP() {
+    return Math.trunc(Math.random() * 9000) + 1000;
+}
 
-let a = 5;
-let b = 4;
-let c = 3;
-
-let s = (a + b + c) / 2;  //s is the semi-perimeter
-console.log(s);
-
-console.log(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
-
-
-/*
-
-a, b, c are the lengths of the sides
-
-s is the semi-perimeter:
-
-Area= 
-s(s−a)(s−b)(s−c)
-
-
-Area= 
-6⋅(6−5)⋅(6−4)⋅(6−3)= 6⋅1⋅2⋅3= 36 =6
-
-*/
+console.log("Your OTP is:", generateOTP());
